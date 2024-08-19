cd2c26d0b336569c9c244cfc7346531bdd53744f
DimaShapoval <dsapoval422@gmail.com>
2024-08-19 09:17:39 +0300

    fixed subscribe

--- src/app/country-page/country-page.component.ts
+++ src/app/country-page/country-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../services/api.service';
 import { CountryInfo } from '../types/app';
@@ -11,23 +11,28 @@ import { CountryInfo } from '../types/app';
   templateUrl: './country-page.component.html',
   styleUrl: './country-page.component.css',
 })
-export class CountryPageComponent implements OnInit {
+export class CountryPageComponent implements OnInit, OnDestroy {
   countryCode!: string | null;
   countryInfo: CountryInfo[] = [];
   countryName: string = '';
   currentYear: number = new Date().getFullYear();
+  subscriber: any;
 
   constructor(
     private activedRoute: ActivatedRoute,
     private apiService: ApiService
   ) {}
 
+  ngOnDestroy(): void {
+    if (this.subscriber) {
+      this.subscriber.unsubscribe();
+    }
+  }
+
   async ngOnInit() {
-    this.activedRoute.paramMap
-      .subscribe((res) => {
-        this.countryCode = res.get('countryCode');
-      })
-      .unsubscribe();
+    this.subscriber = this.activedRoute.paramMap.subscribe((res) => {
+      this.countryCode = res.get('countryCode');
+    });
     if (this.countryCode) {
       this.countryName = await this.apiService.getNameOfCountry(this.countryCode);
       this.countryInfo = await this.apiService.getCountryHolidayInfo(this.countryCode, 2024);
